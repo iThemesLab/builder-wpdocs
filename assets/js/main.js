27e953a408c3440f4e20e4718e3d5dd7d48812c0
Asif Islam <asifpix@gmail.com>
2017-03-30 14:54:43 +0600

    assign homepage added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ $(function(){
 	$('a.ps').bind('click', function(event) {
 		var $anchor = $(this);
 		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top -70
+			scrollTop: $($anchor.attr('href')).offset().top -20
 		}, 1500, 'easeInOutExpo');
 		event.preventDefault();
 	});
-	$('body').scrollspy({ target: '#navbar', offset:72 })
+	$('body').scrollspy({ target: '#navbar', offset:20 })
 });
